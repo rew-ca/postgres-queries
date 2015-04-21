178276fb841587d4267061e4b765a9c30011d79b
AnneKrahnen <akrahnen@glacierinfogroup.com>
2015-04-21 16:12:36 -0700

    Updated Queries

--- copy_dup_id_into_new_properties.sql
+++ copy_dup_id_into_new_properties.sql
@@ -2,9 +2,7 @@
 
 alter table new_properties add column dup_id integer;
 
-select * from new_properties limit 10;
-
 update new_properties 
-set dup_id = u.serial
+set n.dup_id = u.serial
 from new_properties n, unique_addresses u
-where n.new_street_number = u.street_number and n.new_street_name = u.street_name and n.new_street_direction = u.street_direction and n.new_unit_number = u.unit_number and n.city_id = u.city_id
+where n.new_street_number = u.new_street_number and n.new_street_name = u.new_street_name and n.new_street_direction = u.new_street_direction and n.new_unit_number = u.new_unit_number and n.city_id = u.city_id
