404685523d9bb5ec143771044e8a8cae2f360d98
AnneKrahnen <akrahnen@glacierinfogroup.com>
2015-04-21 14:54:43 -0700

    New Queries

---
+++ create_table_new_properties_dup_id.sql
@@ -0,0 +1,7 @@
+﻿create table new_properties_dup_id as 
+(
+select *
+from new_properties
+left join unique_addresses
+using (new_street_number, new_street_name, new_street_direction, new_unit_number, city_id)
+)
---
+++ show_all_duplicates.sql
@@ -0,0 +1,9 @@
+﻿with duplicates as(
+select array_to_string( array [p.new_unit_number, p.new_street_number, p.new_street_name, p.new_street_direction, c.name], ' ' ) as new_address, p.* 
+	from new_properties p, cities c 
+	where p.city_id = c.id
+	and count(*) > 1)
+select new_address, count(*) 
+	from duplicates 
+	group by new_address 
+	having count(*) > 1;
