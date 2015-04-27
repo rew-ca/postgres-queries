af4a913602cdb9f398ef575cc38d5b884244b3ce
AnneKrahnen <akrahnen@glacierinfogroup.com>
2015-04-27 14:48:05 -0700

    Renamed Queries

---
+++ 6_create_new_properties_detailes_fixed.sql
@@ -0,0 +1,61 @@
+﻿drop table new_properties_detailes_fixed;
+
+create table new_properties_detailes_fixed as
+(
+select * from 
+new_properties_dup_id
+left join property_details
+using (dup_id)
+)
+
+update new_properties_detailes_fixed
+	set new_legal_type = legal_type
+	where new_legal_type is null;
+
+update new_properties_detailes_fixed
+	set new_strata_fee = strata_fee
+	where new_strata_fee is null;
+
+update new_properties_detailes_fixed
+	set new_property_tax = property_tax
+	where new_property_tax is null;
+
+update new_properties_detailes_fixed
+	set new_year_built = year_built
+	where new_year_built is null;
+
+update new_properties_detailes_fixed
+	set new_floor_area = floor_area
+	where new_floor_area is null;
+
+update new_properties_detailes_fixed
+	set new_bedroom = bedroom
+	where new_bedroom is null;
+
+update new_properties_detailes_fixed
+	set new bathroom = bathroom
+	where new_bathroom is null;
+
+update new_properties_detailes_fixed
+	set new_assessed_type = assessed_type
+	where new_assessed_type is null;
+
+update new_properties_detailes_fixed
+	set new_lot_size = lot_size
+	where new_lot_size is null;
+
+update new_properties_detailes_fixed
+	set new_den = den
+	where new_den is null;
+
+update new_properties_detailes_fixed
+	new_normalized_type = normalized_type
+	where new_normalized_type is null;
+
+update new_properties_detailes_fixed
+	new_lot_frontage = lot_frontage
+	where new_lot_frontage is null;
+
+update new_properties_detailes_fixed
+	new_lot_depth = lot_depth
+	where new_lot_depth is null;
---
+++ 8_create_new_properties_final.sql
@@ -0,0 +1,46 @@
+﻿create new_properties_final as (
+	select 
+		np.id as np.id,
+		regexp_replace(regexp_replace(regexp_replace(lower(nnp.new_unit_number || '-' || nnp.new_street_number || '-' || nnp.new_street_direction || '-' || nnp.new_street_name || '-' || c.city_name), ' ', '-', 'g'), '--', '-', 'g'), '--', '-', 'g') as slug,
+		regexp_replace(regexp_replace(regexp_replace(lower(nnp.new_street_number || '-' || nnp.new_street_direction || '-' || nnp.new_street_name || '-' || c.city_name), ' ', '-', 'g'), '--', '-', 'g'), '--', '-', 'g') as nnp.parcel_slug,
+		regexp_replace(regexp_replace(regexp_replace(lower(c.city_name || '/' || nnp.new_street_direction || '-' || nnp.new_street_name), ' ', '-', 'g'), '--', '-', 'g'), '--', '-', 'g') as nnp.street_slug,
+		lower(c.city_name || '/' || nnp.postal_code) as nnp.postal_code_slug,
+		nnp.new_street_number as nnp.street_number,
+		np.new_street_name as np.street_name,
+		np.new_street_direction as np.street_direction,
+		np.new_unit_number as np.unit_number,
+		np.city_id as np.city_id,
+		np.postal_code as np.postal_code,
+		(np.new_unit_number || '-' || np.new_street_number || ' ' || np.new_street_name || ', ' c.city_name || ', ' || p.code || ' ' || postal_code || ', ' || co.name) as np.formatted_address,
+		np.latitude as np.latitude,
+		np.longitude as np.longitude,
+		np.geocode_source as np.geocode_source,
+		np.geocode_type as np.geocode_type,
+		np.geocode_status as np.geocode_status,
+		np.new_legal_type as np.legal_type,
+		np.new_strata_fee as np.strata_fee,
+		np.new_property_tax as np.property_tax,
+		np.new_year_built as np.year_built,
+		np.new_floor_area as np.floor_area,
+		np.new_bedroom as np.bedroom,
+		np.new_bathroom as np.bathroom,
+		np.new_assessed_type as np.assessed_type,
+		np.new_lot_size as np.lot_size,
+		np.created_at as np.created_at,
+		np.updated_at as np.updated_at,
+		np.new_den as np.den,
+		np.new_normalized_type as np.normalized_type,
+		np.parcel as np.parcel,
+		np.published as np.published,
+		np.new_lot_frontage as np.lot_frontage,
+		np.new_lot_depth as np.lot_depth,
+		np.use_point_for_street_view as np.use_point_for_street_view,
+		np.matchable as np.matchable,
+		np.uid as np.uid
+		np.dup_id as dup_id
+	from new_properties_details_fixed np, cities c, provinces p, countries co
+	where np.city_id = c.id and 
+		c.province_id = p.id and 
+		p.country_id = co.id
+	)
+	
